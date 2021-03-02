276e10206440489d7dcdc9c914f1195345ac98dc
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-01 21:50:50 -0800

    commit tree 6c7e12735f9be4d11ae7cb8be1c958a9c9a1444f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize (word)
+        @word = word
+    end
+    def match(other_words)
+        matches = []
+        other_words.each do |other_words|
+            
+            if  @word.split("").sort == other_words.split("").sort
+            matches << other_words
+            end
+        end
+        matches
+    end
+end# Your code goes here!
